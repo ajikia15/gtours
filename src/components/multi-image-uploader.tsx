30d661cf9b23f83998f4333be18a0a5addd77b3c
ajikia15 <aleksandrej15@gmail.com>
2025-05-20 19:58:45 +0400

    dnd for uploaded image

--- src/components/multi-image-uploader.tsx
+++ src/components/multi-image-uploader.tsx
@@ -2,7 +2,13 @@
 
 import { useRef } from "react";
 import { Button } from "./ui/button";
-
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "@hello-pangea/dnd";
+import { Badge, MoveIcon, XIcon } from "lucide-react";
 export type ImageUpload = {
   id: string;
   url: string;
@@ -31,19 +37,88 @@ export default function MultiImageUploader({
 
     onImagesChange([...images, ...newImages]);
   };
+  const handleDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(images);
+    const [reorderedImage] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedImage);
+
+    onImagesChange(items);
+  };
+
+  const handleDeleteImage = (id: string) => {
+    const updatedImages = images.filter((image) => image.id !== id);
+    onImagesChange(updatedImages);
+  };
   return (
     <div className="w-full max-w-3xl mx-auto p-4">
       <input
+        className="hidden"
         ref={uploadInputRef}
         type="file"
         multiple
         accept="image/*"
-        className="hidden"
         onChange={handleInputChange}
       />
-      <Button type="button" onClick={() => uploadInputRef?.current?.click()}>
+      <Button
+        className="w-full"
+        variant="outline"
+        type="button"
+        onClick={() => uploadInputRef?.current?.click()}
+      >
         Upload Images
       </Button>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="property-images" direction="vertical">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {images.map((image, index) => (
+                <Draggable key={image.id} draggableId={image.id} index={index}>
+                  {(provided) => (
+                    <div
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      className="relative p-2"
+                    >
+                      <div className="bg-gray-100 rounded-lg flex gap-2 items-center overflow-hidden">
+                        <div className="size-16 relative">
+                          <img
+                            src={image.url}
+                            alt=""
+                            className="object-cover"
+                          />
+                        </div>
+                        <div className="flex-grow">
+                          <p className="text-sm font-medium">
+                            Image {index + 1}
+                          </p>
+                          {index === 0 && <Badge>Featured Image</Badge>}
+                        </div>
+                        <div className="flex items-center p-2">
+                          <button
+                            className="text-red-500 p-2"
+                            onClick={() => handleDeleteImage(image.id)}
+                          >
+                            <XIcon />
+                          </button>
+                          <div className="text-gray-500">
+                            <MoveIcon />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
