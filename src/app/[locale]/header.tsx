955d09d288e0773a2a197d096cd170c2ec409329
ajikia15 <aleksandrej15@gmail.com>
2025-06-17 20:37:16 +0400

    Refactor code structure for improved readability and maintainability

--- src/app/[locale]/header.tsx
+++ src/app/[locale]/header.tsx
@@ -1,7 +1,13 @@
 export default function Header(props: { title: string }) {
   return (
-    <header className="flex justify-center items-center w-full rounded-lg bg-gray-500 h-42">
-      <h1 className="text-white text-4xl font-bold">{props.title}</h1>
+    <header
+      className="flex justify-center items-center w-full rounded-lg bg-gray-500 h-42 bg-cover bg-center bg-no-repeat relative"
+      style={{ backgroundImage: 'url("/header.jpg")' }}
+    >
+      <div className="absolute inset-0 bg-gray-800/70 bg-opacity-50 rounded-lg"></div>
+      <h1 className="text-white text-4xl font-bold relative z-10">
+        {props.title}
+      </h1>
     </header>
   );
 }
